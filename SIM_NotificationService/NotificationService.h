e4deeeef40cb4f2fc5b6e2dff7312573aa1e941e
gaurav2k1 <gaurav.jaimini@gmail.com>
2017-01-20 15:56:21 +0530

    [ShipitMobi] Update Notification Extension.
    - Add Notification Extension Sample for iOS 10 Rich Push Notification Examples.
    - This should be added as a part of embed Binary of the App Project.
    
    Signed-off-by: gaurav2k1 <gaurav.jaimini@gmail.com>

---
+++ SIM_NotificationService/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  SIM_NotificationService
+//
+//  Created by gaurav dwivedi on 18/01/17.
+//  Copyright © 2017 Karmatics. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
